9fd1cf31364f99ec39effd22d713a231e3a751d1
maxinewu5 <48866979+maxinewu5@users.noreply.github.com>
2022-06-26 14:08:14 -0700

    minor code improvements

--- server.js
+++ server.js
@@ -4,49 +4,52 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
+//using config.env to set the variables 
+require("dotenv").config({ path: "./config.env" });
 
+//setup mongo client
 const MongoClient = require('mongodb').MongoClient
 
-//var process.env.MONGODB_URI = "mongodb+srv://maxinewu:SoxDLldTwouxDLYc@cluster0.t34vh.mongodb.net/paper-planes?retryWrites=true";
-//var MONGODB_URI = "mongodb+srv://maxinewu:SoxDLldTwouxDLYc@cluster0.t34vh.mongodb.net/paper-planes?retryWrites=true";
-//var dev_db_url = 'mongodb+srv://maxinewu:NvA0pnjzOrLOlZm1@cluster0.3jbgg.mongodb.net/paper-planes?retryWrites=true&w=majority';
-//var mongoDB = process.env.MONGODB_URI || dev_db_url;
-var mongoDB = "mongodb+srv://maxinewu:SoxDLldTwouxDLYc@cluster0.t34vh.mongodb.net/paper-planes?retryWrites=true";
+var dev_db_url;
+var mongoDB= process.env.MONGODB_URI || dev_db_url;
 console.log("connected to mongodb on " + mongoDB);
+
+var db;
+var messageCollection;
+var curtags = [];
+var usertags = [];
 
 MongoClient.connect(mongoDB, {useUnifiedTopology: true}, (err, client) => {
     if (err) return console.error(err)
     console.log('connected to mongodb')
-    const db = client.db('paper-planes')
-    const messageCollection = db.collection('messages')
-    var curtags = [];
-    var usertags = [];
+    db = client.db('paper-planes')
+    messageCollection = db.collection('messages')
+})
 
-    app.get('/', (req, res) => {
-        //res.render('index.ejs')
-        res.sendFile(__dirname + '/index.html')
-    })
+app.get('/', (req, res) => {
+    //res.render('index.ejs')
+    res.sendFile(__dirname + '/index.html')
+})
 
-    app.post('/plane', (req, res) => {
-        curtags = req.body.tags.split(" ");
-        usertags = usertags.concat(curtags)
-        req.body.tags = curtags;
-        messageCollection.insertOne(req.body) 
-            .then(result => {
-                console.log(result)
-                res.redirect('/')
-            })
-            .catch(error => console.error(error))
-    })
-
-    app.get('/pickup', (req, res) => {
-        console.log("pickup")
-        db.collection('messages').find({"tags":{ $in: usertags} }).toArray()
-        .then(results => {
-            res.render('pickup.ejs', { messages: results})
+app.post('/plane', (req, res) => {
+    curtags = req.body.tags.split(" ");
+    usertags = usertags.concat(curtags)
+    req.body.tags = curtags;
+    messageCollection.insertOne(req.body) 
+        .then(result => {
+            console.log(result)
+            res.redirect('/')
         })
         .catch(error => console.error(error))
+})
+
+app.get('/pickup', (req, res) => {
+    console.log("pickup")
+    db.collection('messages').find({"tags":{ $in: usertags} }).toArray()
+    .then(results => {
+        res.render('pickup.ejs', { messages: results})
     })
+    .catch(error => console.error(error))
 })
 
 app.listen(process.env.PORT || 3000, function() {
